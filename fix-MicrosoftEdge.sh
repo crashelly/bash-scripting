398c17f5a2f43132eef6751371d6bc6a715308c1
crashelly <buitragoagudeloj006@gmail.com>
2024-06-02 11:13:09 -0500

    se crea  el script  de reparar el error de microsoft

---
+++ fix-MicrosoftEdge.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+cd $HOME/.config/microsoft-edge/
+
+echo "moving to microsoft-edge dir..."
+
+echo   "deleting Singleton files "
+rm -f "SingletonCookie"
+rm -f "SingletonSocket"
+rm -f "SingletonLock"
+
+echo "Singleton Files has been deleted"
+echo "open microsoft edge? Y/N"
+# for this  time I finish here  when i can learn more im going to  impove the  code
+
+
